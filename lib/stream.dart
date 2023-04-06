777bd8862adf27905fedc8521c151e672be63899
themw123 <80266862+themw123@users.noreply.github.com>
2023-04-06 21:31:59 +0200

    n

--- lib/stream.dart
+++ lib/stream.dart
@@ -4,7 +4,7 @@ import 'package:notenbenachrichtigung/database.dart';
 import 'package:sqflite/sqflite.dart';
 
 class StreamControllerHelper {
-  static final StreamController<List<List<dynamic>>> _dataStreamController = StreamController<List<List<dynamic>>>();
+  static final StreamController<List<List<dynamic>>> _dataStreamController = StreamController<List<List<dynamic>>>.broadcast();
 
   // Methode, um Daten zu fetchen und sie über den StreamController an die Flutter-Anwendung zu senden
   static void setSubjects() async {
