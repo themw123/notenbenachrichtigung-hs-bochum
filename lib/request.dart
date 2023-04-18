f97fce4e047b90a7d8e271e1315219916545dc00
themw123 <marvin.w6@web.de>
2023-04-18 16:23:47 +0200

    fast fertig

--- lib/request.dart
+++ lib/request.dart
@@ -46,10 +46,7 @@ class Request {
     //künstliche ladezeit
     //await Future.delayed(const Duration(seconds: 3));
 
-    //!!!!!!!!!!!!!!!!!!!!!!wieder einschalten!!!!!!!!!!!!!!!!!!!!!!
-    //bool success = await login();
-    bool success = true;
-    //!!!!!!!!!!!!!!!!!!!!!!wieder einschalten!!!!!!!!!!!!!!!!!!!!!!
+    bool success = await login();
 
     NotificationManager.init();
     if (!success) {
@@ -57,49 +54,28 @@ class Request {
           "Die Noten konnten nicht aktualisiert werden. Login fehlgeschlagen. ");
       return await DatabaseHelper.getSubjects();
     }
-    //!!!!!!!!!!hier subjects von hs bochum holen!!!!!!!!!!!!!!!!!!
 
     /*
-    //cookie setzen
-    var url =
-        'https://studonline.hs-bochum.de/qisserver/rds?state=redirect&sso=qis_mtknr&myre=state%253DexamsinfosStudent%2526next%253Dtree.vm%2526nextdir%253Dqispos/examsinfo/student%2526navigationPosition%253Dfunctions%2CexamsinfosStudent%2526breadcrumb%253Dinfoexams%2526topitem%253Dfunctions%2526subitem%253DexamsinfosStudent%2526asi%$asi';
-
-    var response = await dio.get(url);
-    while (response.statusCode == 302) {
-      url = response.headers.value('location')!;
-      response = await dio.get(url);
-    }
-
-    // asi erneut holen von Seite 'Bitte wählen Sie aus:'
-    var soup = BeautifulSoup(response.data);
-    var pElement = soup.find('p', string: 'Bitte wählen Sie aus:');
-    var parentElement = pElement?.parent;
-    var action = parentElement?['action'];
-    // ignore: prefer_interpolation_to_compose_strings
-    int? start = action?.indexOf('asi=');
-    asi = action?.substring(start! + 4, action.length);
-
-    if (asi == null || asi == '') {
+    //!!!!!!!!!!hier subjects von hs bochum holen!!!!!!!!!!!!!!!!!!
+    dynamic html = await subjectRequest();
+    if (html == false) {
       NotificationManager.showNotification("Test Notification",
           "Die Noten konnten nicht aktualisiert werden. Zweiter Asi konnte nicht ermittelt werden.");
       return await DatabaseHelper.getSubjects();
     }
-
-    // noten holen
-    url =
-        'https://std-info.hs-bochum.de/qisserver/rds?state=examsinfosStudent&next=list.vm&nextdir=qispos/examsinfo/student&createInfos=Y&struct=auswahlBaum&nodeID=auswahlBaum%7Cabschluss%3Aabschl%3D84%2Cstgnr%3D1&expand=1&asi=$asi';
-    response = await dio.get(url);
-
-    var pruefungen = response.data;
+    //!!!!!!!!!!hier subjects von hs bochum holen!!!!!!!!!!!!!!!!!!
     */
-    List<Map<String, dynamic>> subjects = [];
-    var html =
+
+    //simulieren
+    dynamic html =
         '***REMOVED***';
+
     var document = parse(html);
     var tables = document.getElementsByTagName('table');
     var secondTable = tables[1];
     final trs = secondTable.querySelectorAll('tr');
     int counter = 0;
+    List<Map<String, dynamic>> subjects = [];
     for (var tr in trs) {
       if (counter > 1) {
         final tds = tr.querySelectorAll('td');
@@ -120,31 +96,93 @@ class Request {
       }
       counter++;
     }
+
+    //simuliere Notenbenachrichtigung.
+    /*
+    subjects.add({
+      DatabaseHelper.columnSubject: "xx",
+      DatabaseHelper.columnPruefer: "yy",
+      DatabaseHelper.columnDatum: "yx",
+      DatabaseHelper.columnRaum: "xxx",
+      DatabaseHelper.columnUhrzeit: "x",
+      DatabaseHelper.columnOld: 0,
+    });
+    */
+
     List<Map<String, dynamic>> subjectsOld = await DatabaseHelper.getSubjects();
     var newGrades = compare(subjects, subjectsOld);
+
     String newGradesText = "";
-    String text = "Note";
+    counter = 0;
     for (var newGrade in newGrades!) {
-      newGradesText += newGrade['subject'];
+      if (counter != 0) {
+        newGradesText += ", ";
+      }
+      newGradesText += newGrade['fach'];
+      counter++;
     }
+
     if (newGrades.isNotEmpty) {
+      String text = "Note";
       if (newGrades.length > 1) {
         text = "Noten";
       }
       NotificationManager.showNotification(
-          "Neue $text erhalten!", "Du hast neue $text in: $newGradesText .");
+          "Neue $text erhalten!", newGradesText);
     }
-    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    //ergänze noch: wenn neue note in table noten old speichern
-    await DatabaseHelper.setSubjects(subjects);
+
+    //muss immer erfolgen.
+    await DatabaseHelper.removeAllSubjects();
+    await DatabaseHelper.setSubjects(DatabaseHelper.tableNoten, subjects);
+    await DatabaseHelper.setSubjects(DatabaseHelper.tableNotenOld, newGrades);
     return await DatabaseHelper.getSubjects();
+  }
+
+  Future<dynamic> subjectRequest() async {
+    //cookie setzen
+    var url =
+        'https://studonline.hs-bochum.de/qisserver/rds?state=redirect&sso=qis_mtknr&myre=state%253DexamsinfosStudent%2526next%253Dtree.vm%2526nextdir%253Dqispos/examsinfo/student%2526navigationPosition%253Dfunctions%2CexamsinfosStudent%2526breadcrumb%253Dinfoexams%2526topitem%253Dfunctions%2526subitem%253DexamsinfosStudent%2526asi%$asi';
+
+    var response = await dio.get(url);
+    while (response.statusCode == 302) {
+      url = response.headers.value('location')!;
+      response = await dio.get(url);
+    }
+
+    // asi erneut holen von Seite 'Bitte wählen Sie aus:'
+    var soup = BeautifulSoup(response.data);
+    var pElement = soup.find('p', string: 'Bitte wählen Sie aus:');
+    var parentElement = pElement?.parent;
+    var action = parentElement?['action'];
+    // ignore: prefer_interpolation_to_compose_strings
+    int? start = action?.indexOf('asi=');
+    asi = action?.substring(start! + 4, action.length);
+
+    if (asi == null || asi == '') {
+      return Future.value(false);
+    }
+
+    // noten holen
+    url =
+        'https://std-info.hs-bochum.de/qisserver/rds?state=examsinfosStudent&next=list.vm&nextdir=qispos/examsinfo/student&createInfos=Y&struct=auswahlBaum&nodeID=auswahlBaum%7Cabschluss%3Aabschl%3D84%2Cstgnr%3D1&expand=1&asi=$asi';
+    response = await dio.get(url);
+
+    return response.data;
   }
 
   List<Map<String, dynamic>>? compare(List<Map<String, dynamic>> subjects,
       List<Map<String, dynamic>> subjectsOld) {
     List<Map<String, dynamic>> newGrades = [];
     for (var subjectOld in subjectsOld) {
-      if (!subjects.contains(subjectOld)) {
+      bool found = false;
+      for (var subject in subjects) {
+        if (subject["fach"] == subjectOld["fach"]) {
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        subjectOld['old'] = 1;
         newGrades.add(subjectOld);
       }
     }
@@ -152,11 +190,10 @@ class Request {
   }
 
   Future<bool> login() async {
-    /*
     await Future.delayed(const Duration(seconds: 3));
-    return false;
-    */
+    return Future.value(true);
 
+    /*
     // Startseite
     String url =
         "https://studonline.hs-bochum.de/qisserver/rds?state=user&type=0";
@@ -204,5 +241,6 @@ class Request {
     }
 
     return Future.value(true);
+    */
   }
 }
