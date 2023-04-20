3b01d77fb89df60d119d7772be6f4e109e1d5ac7
themw <marvin.w6@web.de>
2023-04-20 12:41:34 +0200

    n

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 import Flutter
+//von mir
+import workmanager
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +11,28 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    //von mir
+    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(3600))
+    UNUserNotificationCenter.current().delegate = self
+    WorkmanagerPlugin.setPluginRegistrantCallback { registry in
+        // Registry in this case is the FlutterEngine that is created in Workmanager's
+        // performFetchWithCompletionHandler or BGAppRefreshTask.
+        // This will make other plugins available during a background operation.
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+    WorkmanagerPlugin.registerTask(withIdentifier: "meintask")
+    //von mir
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  //von mir
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      completionHandler(.alert) // shows banner even if app is in foreground
+  }
+  //von mir
 }
